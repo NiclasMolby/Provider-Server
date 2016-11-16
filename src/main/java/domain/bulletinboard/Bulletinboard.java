ba7d3f214d00c0f65f71c0618c4452b8aa96020d
Niels <Niels@NielsLaptop>
2016-11-16 15:47:49 +0100

    Bulletinboard overført fra c#

---
+++ src/main/java/domain/bulletinboard/Bulletinboard.java
@@ -0,0 +1,54 @@
+package domain.bulletinboard;
+
+import domain.util.PostTypes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bulletinboard {
+
+    private List<Post> posts;
+
+    public Bulletinboard() {
+        posts = new ArrayList<>();
+    }
+
+    public void createPost(String owner, String title, String description, PostTypes type) {
+        Post post = new Post(owner, title, description, type);
+        //post.setId(Database.instance.AddPost(owner, post)); //TODO: hent id fra databasen når en post bliver oprettet
+        posts.add(post);
+    }
+
+    public void deletePost(Post post) {
+        //Database.instance.DeletePost(post); // TODO: slet post fra databasen
+        posts.remove(post);
+    }
+
+    public void editPost(Post post, String newDescription, String newTitle) {
+        Post postFound = posts.stream().filter(p -> p == post).findFirst().get();
+        postFound.setDescription(newDescription);
+        postFound.setTitle(newTitle);
+        //Database.instance.UpdatePost(post.owner, post); //TODO: opdater posten i databasen
+    }
+
+    private List<Post> getPosts(PostTypes type) {
+        return posts.stream().filter(p -> p.getType() == type).collect(Collectors.toList());
+    }
+
+    public List<Post> viewAllPosts() {
+        return posts;
+    }
+
+    public List<Post> viewWarningPosts() {
+        return getPosts(PostTypes.Warning);
+    }
+
+    public List<Post> viewRequestPosts() {
+        return getPosts(PostTypes.Request);
+    }
+
+    public List<Post> viewOfferPosts() {
+        return getPosts(PostTypes.Offer);
+    }
+
+}
