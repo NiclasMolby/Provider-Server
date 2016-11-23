cb3a23f37d7da43dcad5c6601a174c71f8ace5aa
Karim <karimkuku@gmail.com>
2016-11-23 20:20:45 +0100

    new and better without errors :D(hopfully)

--- src/main/java/io/swagger/api/impl/ControllerApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/ControllerApiServiceImpl.java
@@ -1,14 +1,15 @@
 package io.swagger.api.impl;
 
+import domain.controller.Controller;
 import io.swagger.api.*;
 import io.swagger.model.*;
 
 import io.swagger.model.PostType;
 import io.swagger.model.Post;
-import java.util.Date;
 import io.swagger.model.Page;
 import io.swagger.model.User;
 
+import java.util.Date;
 import java.util.List;
 import io.swagger.api.NotFoundException;
 
@@ -19,22 +20,12 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-11-23T18:03:00.493Z")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-11-23T11:49:43.002Z")
 public class ControllerApiServiceImpl extends ControllerApiService {
     @Override
     public Response addNoteToSupplier(String supplierName, String editor, String text, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
-    @Override
-    public Response createPost(String owner, Date date, String title, String description, PostType type, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
-    @Override
-    public Response deletePost(Post post, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        Controller.getController().addNoteToSupplier(supplierName, editor, text);
+        return Response.ok().build();
     }
     @Override
     public Response editPost(Post post, String newDescription, String newTitle, SecurityContext securityContext) throws NotFoundException {
@@ -43,17 +34,26 @@ public class ControllerApiServiceImpl extends ControllerApiService {
     }
     @Override
     public Response getAllPosts(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(Controller.getController().getAllPosts()).build();
     }
     @Override
     public Response getSupplier(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(Controller.getController().getSuppliers()).build();
     }
     @Override
     public Response validate(String username, String password, SecurityContext securityContext) throws NotFoundException {
+        return Response.ok().entity(Controller.getController().validate(username, password)).build();
+    }
+    @Override
+    public Response createPost(String owner, String title, String description, PostType type, SecurityContext securityContext) throws NotFoundException {
+
+        return Response.ok().entity(Controller.getController().createPost(owner, title, description, type)).build();
+    }
+
+    @Override
+    public Response deletePost(Post post, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
 }
+
