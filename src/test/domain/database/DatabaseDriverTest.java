e2f6876801587d7ac7a88eab566521b0b15a73ca
Karim <karimkuku@gmail.com>
2016-12-08 12:23:34 +0100

    Unit test

--- src/test/domain/database/DatabaseDriverTest.java
+++ src/test/domain/database/DatabaseDriverTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,20 +16,26 @@ public class DatabaseDriverTest {
 
     private DatabaseDriver database;
     private Post testPost;
+    private Product testProduct;
 
     @Before
     public void setUp() throws Exception {
         database = DatabaseDriver.getInstance();
         testPost = new Post().type(PostType.OFFER).title("test").description("testDescription").owner("Test").date(new Date().toString());
+        testProduct = new Product().productName("Test").chemicalName("Test").deliveryTime("Test").description("Test").producer("BobSagat").price(4.20).molWeight(4.20).packaging("test");
     }
 
     @After
     public void tearDown() throws Exception {
-        if(database.getPosts().contains(testPost)) {
+        if (database.getPosts().contains(testPost)) {
             database.deletePost(testPost);
+        }
+        if (database.getProducts("Bobsagat").contains(testProduct)) {
+            database.deleteProduct(testProduct);
         }
         database = null;
         testPost = null;
+        testProduct = null;
     }
 
     @Test
@@ -45,10 +52,9 @@ public class DatabaseDriverTest {
     @Test
     public void getSuppliersTest() throws Exception {
         List<Page> p = new ArrayList<>();
-        if(database.getSuppliers().isEmpty()) {
+        if (database.getSuppliers().isEmpty()) {
             fail();
-        }
-        else {
+        } else {
             assertEquals(p.getClass(), database.getSuppliers().getClass());
         }
     }
@@ -56,10 +62,9 @@ public class DatabaseDriverTest {
     @Test
     public void getProductsTest() throws Exception {
         List<Product> p = new ArrayList<>();
-        if(database.getProducts("BobSagat").isEmpty()) {
+        if (database.getProducts("BobSagat").isEmpty()) {
             fail();
-        }
-        else {
+        } else {
             assertEquals(p.getClass(), database.getSuppliers().getClass());
         }
     }
@@ -67,55 +72,75 @@ public class DatabaseDriverTest {
     @Test
     public void getPostsTest() throws Exception {
         List<Post> p = new ArrayList<>();
-        if(database.getPosts().isEmpty()) {
-            fail();
-        }
-        else {
+        if (database.getPosts().isEmpty()) {
+            fail("Der er ikke nogen post i databasen");
+        } else {
             assertEquals(p.getClass(), database.getSuppliers().getClass());
         }
     }
 
     @Test
-    public void addAndDeletePostTest() throws Exception {
-        addPostTest();
-        deletePostTest();
-    }
-
     public void addPostTest() throws Exception {
-        if(database.getPosts().contains(testPost)) {
+
+        if (database.getPosts().contains(testPost)) {
             fail("There is already a test post in the database");
         }
         testPost.id(database.addPost("Test", testPost));
-        if(!database.getPosts().contains(testPost)) {
+        if (!database.getPosts().contains(testPost)) {
             fail("The post was not added to the database!");
         }
+        database.deletePost(testPost);
     }
 
+    @Test
     public void deletePostTest() throws Exception {
-        if(!database.getPosts().contains(testPost)) {
+        testPost.id(database.addPost("Test", testPost));
+        if (!database.getPosts().contains(testPost)) {
             fail("The post was never created in the database");
         }
         database.deletePost(testPost);
-        for(Post p : database.getPosts()) {
-            if(p.equals(testPost)) {
+        for (Post p : database.getPosts()) {
+            if (p.equals(testPost)) {
                 fail("The post was not deleted");
             }
         }
     }
 
     @Test
-    public void deleteProduct() throws Exception {
-        //TODO
+    public void addProduct() throws Exception {
+        testProduct.id(database.addProduct(testProduct));
+        database.addProductToPage(testProduct);
+        if (!database.getProducts("BobSagat").contains(testProduct)) {
+            fail("The post was not added to the database!");
+        }
+        database.deleteProduct(testProduct);
     }
 
     @Test
-    public void addProduct() throws Exception {
-        //TODO
+    public void deleteProduct() throws Exception {
+        if (database.getProducts("BobSagat").contains(testProduct)) {
+            fail("There is already a test product in the database");
+        }
+        testProduct.id(database.addProduct(testProduct));
+        database.addProductToPage(testProduct);
+        if (!database.getProducts("BobSagat").contains(testProduct)) {
+            fail("The post was not added to the database!");
+        }
+        database.deleteProduct(testProduct);
+        if (database.getProducts("BobSagat").contains(testProduct)) {
+            fail("The post was deleted from the database!");
+        }
     }
 
     @Test
     public void getPDFFilePath() throws Exception {
-        //TODO
+        database.getPDFFilePath(1);
+        assertEquals("".getClass(), database.getPDFFilePath(1).getClass());
     }
 
+    @Test
+    public void isFileOnPDFpath() throws Exception {
+        database.getPDFFilePath(1);
+        assertTrue(new File(database.getPDFFilePath(1)).exists());
+    }
 }
